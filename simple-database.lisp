cc3a1124c4452b83788e1e46bc3635737ba37d1e
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-02-20 00:35:57 -0300

    Add prompts to add cd, persist db in file and load file to a *db* variable

--- simple-database.lisp
+++ simple-database.lisp
@@ -15,4 +15,31 @@
 (add-record (make-cd "arte do insulto" "matanza" 9 nil))
 (add-record (make-cd "odiosa natureza humana" "matanza" 10 nil))
 
-(dump-db)
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: "))
+         (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
